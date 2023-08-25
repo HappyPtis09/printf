47b779d3955b9fb029d4bc830f0c63bc925c62b2
HappyPtis09 <bokunozaq@gmail.com>
2023-08-25 15:45:25 +0000

    Clean and well

--- params.c
+++ params.c
@@ -14,16 +14,17 @@ void init_params(params_t *params, va_list list)
 {
 	params->unsign = 0;
 
+	params->zero_flag = 0;
 	params->plus_flag = 0;
+	params->minus_flag = 0;
 	params->space_flag = 0;
 	params->hashtag_flag = 0;
-	params->zero_flag = 0;
-	params->minus_flag = 0;
 
 	params->width = 0;
 	params->precision = UINT_MAX;
 
 	params->h_modifier = 0;
 	params->l_modifier = 0;
+
 	(void)list;
 }
