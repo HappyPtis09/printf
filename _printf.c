37fec50ce4cd393916f1fdf152aa9a1472aca7b5
HappyPtis09 <bokunozaq@gmail.com>
2023-08-25 15:15:20 +0000

    update _printf

--- _printf.c
+++ _printf.c
@@ -6,20 +6,25 @@
  * @format: string's format
  *
  * Return: int
+ *
  */
 int _printf(const char *format, ...)
 {
 	int summ = 0;
 	va_list list;
 	char *p, *start;
-	params_t params = PARAMS_INIT;
+	params_t params = PARAMSINIT;
 
 	va_start(list, format);
 
 	if (!format || (format[0] == '%' && !format[1]))
+	{
 		return (-1);
+	}
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
+	{
 		return (-1);
+	}
 	for (p = (char *)format; *p; p++)
 	{
 		init_params(&params, list);
@@ -37,14 +42,21 @@ int _printf(const char *format, ...)
 		p = get_width(p, &params, list);
 		p = get_precision(p, &params, list);
 		if (get_modifier(p, &params))
+		{
 			p++;
+		}
 		if (!get_specifier(p))
+		{
 			summ += print_from_to(start, p,
 				params.l_modifier || params.h_modifier ? p - 1 : 0);
+		}
 		else
+		{
 			summ += get_print_func(p, list, &params);
+		}
 	}
-	_putchar(BUF_FLUSH);
+
+	_putchar(BUFFFLUSH);
 	va_end(list);
 	return (summ);
 }
